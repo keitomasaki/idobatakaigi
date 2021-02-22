5804f034506499586932ed30fbcec313deba28c5
masaki <keitomasaki@icloud.com>
2021-02-22 23:23:00 +0900

    kkk

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,23 @@
 import firebase from "firebase";
+// const {
+//   REACT_APP_FIREBASE_API_KEY,
+//   REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   REACT_APP_FIREBASE_DATABASE_URL,
+//   REACT_APP_FIREBASE_PROJECT_ID,
+//   REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   REACT_APP_FIREBASE_APP_ID,
+// } = process.env;
+
+// const firebaseConfig = {
+//   apiKey: REACT_APP_FIREBASE_API_KEY,
+//   authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+//   projectId: REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: REACT_APP_FIREBASE_APP_ID,
+// };
 
 const firebaseConfig = {
   apiKey: "AIzaSyC0SfQTaVJCNUh-lXa3BNJJJ4z21_WSWBY",
